f4257868a2d40d5bf2e3e24f8a34f9362258181e
OutrageousPotato <jonathan@gibsonnotes.com>
2017-11-04 16:48:28 -0700

    Add project files.

---
+++ KnockoutMvc/scripts/club.js
@@ -0,0 +1,25 @@
+﻿function Member(name, number, food) {
+    var self = this;
+    self.Name = ko.observable(name);
+    self.PhoneNumber = ko.observable(number);
+    self.Food = ko.observable(food);
+}
+
+function MemberModel() {
+    var self = this;
+    self.MemberList = ko.observableArray([
+        new Member("Abraham Lincoln", "503-754-8096", "Pizza"),
+        new Member("Ted Danson", "816-931-3548", "Poison")
+    ]);
+
+    self.remove = function () {
+        self.MemberList.remove(this);
+    };
+    self.add = function() {
+        self.MemberList.push(new Member("name", "number", "food"));
+    };
+}
+$(function () {
+    var _vm = new MemberModel();
+    ko.applyBindings(_vm);
+});
